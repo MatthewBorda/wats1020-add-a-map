31629856e27c99a990481451a0f3b6a4f6607b19
matthewmborda <matthewmborda@gmail.com>
2016-01-27 18:27:46 -0500

    Functioning map

--- js/main.js
+++ js/main.js
@@ -8,16 +8,62 @@
 */
 $( document  ).ready(function() {
 	$( "#iPicture" ).iPicture();
-  var map = L.map('map', {
-    center: [46.8529, -121.7604],
-    zoom: 13
+// 	I think I like the non-popup better?
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+// 	place holder for LL to pull tile
+var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+
+//Open street map layer	
+var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+
+var terrainUrl = 'http://tile.stamen.com/terrain-background/{z}/{x}/{y}.jpg'
+var terrainAttrib = "Test"
+var terrain = new L.TileLayer(terrainUrl, {minZoom: 8, maxZoom: 18, attribution: terrainAttrib});
+
+var topoUrl = "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png"
+var topoAttrib = "Topo"
+var topo = new L.TileLayer(topoUrl, {minZoom: 8, maxZoom: 18, attribution: topoAttrib});
+//Satelite layer
+var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
 });
-L.tileLayer('https://api.mapbox.com/styles/v1/matthewmborda/cijt62sxg00938zkqirobk93q.html?title=true&access_token=pk.eyJ1IjoibWF0dGhld21ib3JkYSIsImEiOiJjaWp0NXRhd2Ywa2JidW9seDlvNWFvMG41In0.EydtNiNzSblstHwBX_MIdQ#9.82/46.852900000000005/-121.76039999999998/0', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'matthewmborda',
-    accessToken: 'pk.eyJ1IjoibWF0dGhld21ib3JkYSIsImEiOiJjaWp0NXRhd2Ywa2JidW9seDlvNWFvMG41In0.EydtNiNzSblstHwBX_MIdQ'
-}).addTo(map);
+//Drawing layer
+var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+});
+	
+// define layers
+var mapLayers = {
+    "Satellite": satLayer,
+    "Map View": drawLayer,
+    "Open Street Maps": osm,
+		"Terrain View": terrain,
+		"Topographic View": topo
+} 
+
+//Set view of map
+var map = L.map('map-container').setView([46.852, -121.760], 10);
+// Layer control box
+L.control.layers(mapLayers).addTo(map);
+satLayer.addTo(map);
+
+var markerPeak = L.marker([46.852, -121.760]).addTo(map);
+
+markerPeak.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+var markerParadise = L.marker([46.790, -121.740]).addTo(map);
+markerParadise.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+var markerSunrise = L.marker([46.750, -121.810]).addTo(map);
+	markerSunrise.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+var markerLongmire = L.marker([46.910, -121.640]).addTo(map);
+	markerLongmire.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+// 	Add links to web cams
+// Carbon riverLatitude: 46.9948269
+// Longitude: -121.9153839
+	// Paradise (46.79°N 121.74°W[18]) 
+// Longmire (46.75°N 121.81°W)
+// Sunrise (46.91°N 121.64°W)
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
 });
@@ -30,3 +76,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/matthewmborda/cijt62sxg00938zkqiro
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 
+// var marker = L.marker([46.852, -121.760]).addTo(map);
+// marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
